3e73978d76c6fdea566959b74f8a64b7235387d1
oscarrezab <110639308+oscarrezab@users.noreply.github.com>
2023-10-31 17:29:21 -0500

    Fixed actions
    Added functionality for mobile devices

--- PasswordManager/main.js
+++ PasswordManager/main.js
@@ -1,17 +1,24 @@
 // display modal on click
 
 const modalWrapper = document.querySelector(".modals-wrapper");
-function displayModal(id){
-    const modal = document.getElementById(id);
-    modalWrapper.style.display = "flex";
-    modal.style.display = "flex";
-    // close modal
-    const close = document.getElementById("close-modal");
-    close.addEventListener("click", () =>{
-        modalWrapper.style.display = "none";
-        modal.style.display = "none";    
-    })
+if (modalWrapper){
+    function displayModal(id){
+        const modal = document.getElementById(id);
+        modalWrapper.style.display = "flex";
+        modal.style.display = "flex";
+        // close modal
+        const close = document.getElementById("close-modal");
+        close.addEventListener("click", () =>{
+            modalWrapper.style.display = "none";
+            modal.style.display = "none"; 
+            document.querySelector("header").style.display = "unset";
+            document.querySelector("footer").style.display = "unset";    
+        })
+        document.querySelector("header").style.display = "none";
+        document.querySelector("footer").style.display = "none";
+    }
 }
+
 
 // copy elements
 function copyEmail() {
@@ -26,3 +33,20 @@ function copyPass() {
     navigator.clipboard.writeText(textToCopy);
 };
 
+
+// Dsplay actions of password card for mobile devices
+const actions = document.querySelectorAll(".actions");
+if (actions){
+    actions.forEach(action =>{
+        action.onclick = () =>{
+            const links = action.querySelectorAll("a");
+            links.forEach(link =>{
+                link.style.display = "flex";
+            })
+            setTimeout(function(){
+                links.forEach(link =>{
+                    link.style.display = "none";
+                })}, 3000)
+        }
+    })
+}
