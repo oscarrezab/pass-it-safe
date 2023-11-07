ef8068debfc8bf19ca2b1af4767482b0f7caec06
oscarrezab <110639308+oscarrezab@users.noreply.github.com>
2023-11-06 20:55:37 -0600

    Send email
    Success in avoiding to hard-code the credentials

--- passitsafe/passitsafe/settings.py
+++ passitsafe/passitsafe/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,6 +129,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "email@gmail.com"
-EMAIL_HOST_PASSWORD = "password"
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
